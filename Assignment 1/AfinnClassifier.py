f8d9d5233d09d0f51d23c575a6969a48caff2ee0
Chia Wei Meng Alexander <chiaweimeng.alexander@gmail.com>
2017-02-25 20:23:07 +0800

    use Afinn python package instead of text file

--- Assignment 1/AfinnClassifier.py
+++ Assignment 1/AfinnClassifier.py
@@ -1,5 +1,6 @@
 from nltk.tokenize import TweetTokenizer
 from Classifier import Classifier
+from afinn import Afinn
 import Tweets
 import operator
 
@@ -9,28 +10,20 @@ class AfinnTweetClassifier(Classifier):
 		self._tknzr = TweetTokenizer(strip_handles=True)
 		self._tweets = Tweets.TrainTweets()
 		self._afinn = {}
-		self._train()
+		self._afinn = Afinn(emoticons=True)
 
 	def _tokenize(self, tweet):
 		return [token for token in self._tknzr.tokenize(tweet['text'])]
-
-	def _load_afinn(self):
-		afinn_file = open("AFINN-111.txt", "r")
-		for term in afinn_file:
-			term = term.split()
-			self._afinn["".join(term[:-1])] = int(term[-1])
-
-	def _train(self):
-		self._load_afinn()
 
 	def _score_words(self, tweet):
 		tweet_tokens = self._tokenize(tweet)
 		prob = {"positive": 0, "negative": 0, "neutral": 0}
 		for token in tweet_tokens:
-			if token in self._afinn:
-				score = self._afinn[token]
-				if score > 0: prob["positive"] += score
-				else: prob["negative"] += (-score)
+			score = self._afinn.score(token)
+			if score > 0: 
+				prob["positive"] += score
+			elif score < 0: 
+				prob["negative"] += (-score)
 			else:
 				prob["neutral"] += 0.1
 		return prob
