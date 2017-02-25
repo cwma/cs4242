4afe255923acc94e5f4cc73e2f5ad5a808e6ded4
Chia Wei Meng Alexander <chiaweimeng.alexander@gmail.com>
2017-02-25 20:18:42 +0800

    added caching

--- Assignment 1/Tweets.py
+++ Assignment 1/Tweets.py
@@ -1,22 +1,27 @@
 import json
 
+cache = {}
 
 def Tweets(index_file):
-    index = json.load(open(index_file, 'r'))
-    tweets = {}
-    for tweet_id, tweet in index.items():
-        raw_tweet = open("dataset/" + tweet['text'], 'r', encoding='utf-8').read()
-        raw_tweet = json.loads(raw_tweet)
-        tweet['id'] = tweet_id
-        tweet['filename'] = tweet['text']
-        tweet['text'] = raw_tweet['text'].lower()
-        tweet['rt'] = raw_tweet['retweet_count']
-        tweet['fav'] = raw_tweet['favorite_count']
-        tweet['user_desc'] = raw_tweet['user']['description'].lower()
-        tweet['followers'] = raw_tweet['user']['followers_count']
-        tweet['userid'] = raw_tweet['user']['id']
-        tweets[tweet_id] = tweet
-    return tweets
+    if index_file in cache:
+        return cache[index_file]
+    else:
+        index = json.load(open(index_file, 'r'))
+        tweets = {}
+        for tweet_id, tweet in index.items():
+            raw_tweet = open("dataset/" + tweet['text'], 'r', encoding='utf-8').read()
+            raw_tweet = json.loads(raw_tweet)
+            tweet['id'] = tweet_id
+            tweet['filename'] = tweet['text']
+            tweet['text'] = raw_tweet['text'].lower()
+            tweet['rt'] = raw_tweet['retweet_count']
+            tweet['fav'] = raw_tweet['favorite_count']
+            tweet['user_desc'] = raw_tweet['user']['description'].lower()
+            tweet['followers'] = raw_tweet['user']['followers_count']
+            tweet['userid'] = raw_tweet['user']['id']
+            tweets[tweet_id] = tweet
+        cache[index_file] = tweets
+        return tweets
 
 
 def TrainTweets():
