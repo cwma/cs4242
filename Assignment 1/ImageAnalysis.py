7dea68c7d714514e86cbc4a27f58564ec7191a5c
Chia Wei Meng Alexander <chiaweimeng.alexander@gmail.com>
2017-02-26 00:06:13 +0800

    added save file handling

--- Assignment 1/ImageAnalysis.py
+++ Assignment 1/ImageAnalysis.py
@@ -1,9 +1,15 @@
 import os
+import json
 
 from CognitiveServices import ComputerVision, Emotion
 
 
 class ImageAnalysis():
+
+    def save_file(self, result, file_name):
+        _file = open(file_name, "w")
+        _file.write(json.dumps(result))
+
     def run_computer_vision(self):
         computer_vision_key = '288662f5a12f4e2cb81270bcc9ecf35f'
         captions_list = []
@@ -19,8 +25,10 @@ class ImageAnalysis():
 
             result = computer_vision.analyze_image(data, 'Tags, Description')
 
-            image_label.append(r'./dataset/photos/' + infile)
-            captions_list.append(result['description']['captions'][0]['text'])
+            #image_label.append(r'./dataset/photos/' + infile)
+            #captions_list.append(result['description']['captions'][0]['text'])
+            _result = { r'photos/' + infile: { "description": result['description']['captions'][0]['text'], "tags": result['description']['tags']}}
+            self.save_file(_result, 'dataset/vision/{0}.json'.format(infile))
 
         return captions_list, image_label
 
@@ -49,8 +57,13 @@ class ImageAnalysis():
                 # print(face['scores'])
                 print(emotion_list)
 
+            _result = { r'photos/' + infile: [{key: value for key, value in zip(emotion_label, emotion_sublist)} for emotion_sublist in emotion_list]}
+            self.save_file(_result, 'dataset/emotion/{0}.json'.format(infile))
+
         return emotion_list, emotion_label
 
-    if __name__ == '__main__':
-        run_computer_vision()
-        run_emotion()
+if __name__ == '__main__':
+
+    ia = ImageAnalysis()
+    ia.run_computer_vision()
+    ia.run_emotion()
